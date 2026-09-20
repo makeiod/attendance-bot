d8737506df0ef09efe05146e6a7569d64b74eab0
Makeio <desadiermakeio@gmail.com>
2026-09-19 21:03:19 -0700

    actually actually improved graph

--- analytics.py
+++ analytics.py
@@ -9,8 +9,6 @@ async def simple(user_id, timeframe, type):
     """Returns a simple line graph of a users attendance percentage throughout the year since joining the team"""
     percentages = await database.get_percentages(timeframe, user_id, type)
     plt.style.use('dark_background')
-    plt.xlabel('Practice')
-    plt.ylabel('Attendance Percent')
     fig, ax = plt.subplots()
     x, y = [], []
     i = 1
@@ -18,6 +16,8 @@ async def simple(user_id, timeframe, type):
         x.append(f'{i:02d}')
         y.append(percentages[key])
         i += 1
+    plt.xlabel('Practice')
+    plt.ylabel('Attendance Percent')
     plt.ylim(0, 105)
     ax.plot(x, y, marker='o', color='#002855', markerfacecolor='#B3A369')
     plt.savefig(f'{user_id}_report.png')
