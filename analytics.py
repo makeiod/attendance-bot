8e930ec671ed520a2be316e34330f057d9a382a1
Makeio <desadiermakeio@gmail.com>
2026-09-19 21:18:30 -0700

    GRAPH

--- analytics.py
+++ analytics.py
@@ -12,6 +12,11 @@ async def simple(user_id, timeframe, type):
     ax.tick_params(colors='white')
     ax.xaxis.label.set_color('white')
     ax.yaxis.label.set_color('white')   
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_color('white')
+    ax.spines['left'].set_color('white')
+    ax.grid(color='gray', linestyle='--', alpha=0.3)
     x, y = [], []
     i = 1
     for key in percentages:
@@ -21,7 +26,7 @@ async def simple(user_id, timeframe, type):
     plt.xlabel('Practice')
     plt.ylabel('Attendance Percent')
     plt.ylim(0, 105)
-    ax.plot(x, y, marker='o', markerfacecolor='#002855', color='#B3A369', linewidth=3)
+    ax.plot(x, y, marker='o', markerfacecolor='white', color='#B3A369', linewidth=3)
     plt.savefig(f'{user_id}_report.png', transparent=True)
     plt.close()
     return f'{user_id}_report.png'
